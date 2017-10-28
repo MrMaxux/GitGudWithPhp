74a21fbdfe8cc6021df00b55ca1c56153f600724
Maxux <maxux@ymail.com>
2017-10-28 19:43:20 +0300

    Step 4, Level 1

--- Level_1/Step4/Step4.php
+++ Level_1/Step4/Step4.php
@@ -16,7 +16,7 @@
 
       <h1>
         <?php
-        if($_GET['name' !== '' ]){
+        if(isset($_GET['name'])){
            $name = $_GET['name'];
            echo 'Hello, '.$name;
         } else{
